329e3c9abc882e823a00432eab7fa3bbb551db16
JohnM <mars.john2018@gmail.com>
2022-06-08 21:51:05 -0400

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title></title>
+        <meta name='description' content='' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1 class='text-6xl text-center'>Welcome!</h1>
+    </div>
+  )
+}
